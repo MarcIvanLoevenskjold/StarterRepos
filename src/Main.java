9e31b0c464022ec81e32bcde0d2ff45167094084
MarcIvanLoevenskjold <mil@ruc.dk>
2020-02-18 15:16:47 +0100

    my first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+
+
+        try {
+            int num = Integer.parseInt(args[0]);
+            for (int i = 1; i <= 10; i++) {
+                int result = i * num;
+                System.out.println(num + " x " + i + " = " + result);
+
+            } 
+        } catch (NumberFormatException e){
+            System.out.println("input need to be an integer between 1 and 10");
+        }
+    }
+
+
+}
